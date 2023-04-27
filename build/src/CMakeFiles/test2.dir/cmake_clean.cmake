63003432932d9ad8843b946e51ea3799c129b23d
yxwwindows <1499058290@qq.com>
2023-04-27 16:21:44 +0800

    update:update test test2
    Signed-off-by: yxwwindows <1499058290@qq.com>

---
+++ build/src/CMakeFiles/test2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test2.dir/main.cpp.obj"
+  "CMakeFiles/test2.dir/main.cpp.obj.d"
+  "libtest2.dll.a"
+  "test2.exe"
+  "test2.exe.manifest"
+  "test2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
